ae78084b42e178c2ee2ac1cf985604997fd3885e
ASH <25776660+iash8090@users.noreply.github.com>
2023-09-09 12:16:01 +0530

    Update settings.py

--- quora/settings.py
+++ quora/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-cn3wwtnd43=k2xde#v6#686aj+e)d(to^7x@5d4)t-*ta!lgcg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
